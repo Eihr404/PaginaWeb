5767803ac08f9144256abf224cac69a7a9d0fdd5
Eihr404 <israhernandezr@gmail.com>
2025-11-05 22:40:52 -0500

    pruebas jquery

---
+++ js/funciones/JQuery.js
@@ -0,0 +1,13 @@
+/*Contactos e Informacion*/
+
+$(document).ready(function(){
+  $("#btnContacto").on("click", function(){
+    const cuadro = $("#cuadro");
+    cuadro.html("<p><strong>Cargando...</strong></p>").fadeIn(300);
+
+    // Simulamos carga de información
+    setTimeout(() => {
+      cuadro.html("<p>Información de contacto: <br> Tel: 0999999999</p>");
+    }, 2000);
+  });
+});
