bcead0555e9b21039b28fe893390b240d26b2654
Eihr404 <israhernandezr@gmail.com>
2025-11-06 21:20:13 -0500

    cambios en la distribucion de productos, index funciona ahora

--- js/funciones/Funciones.js
+++ js/funciones/Funciones.js
@@ -14,8 +14,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const nombre=categoria.nombre[0];
     const descripcion=categoria.descripcion[0];
     const imagen= categoria.imagen[0];
-    const precio= categoria.precios[0];
-
     const card= document.createElement("div");
     card.classList.add("col-md-3","mb-4");
 
@@ -24,38 +22,44 @@ document.addEventListener("DOMContentLoaded", ()=>{
         <img src="${imagen}" class="card-img-top" alt="${nombre}">
         <div class="card-body">
           <h5 class="card-title">${nombre}</h5>
-          <p class="card-text">${descripcion}</p>
-          <p class="card-text">${precio}</p>
+          <p class="card-text">${descripcion}</p>       
         </div>
       </div>
     `;
     contenedorGrid.appendChild(card);
   }
-  /*Funcion e ID para el carrusel*/
 
+});
+
+document.addEventListener("DOMContentLoaded", ()=>{
   const contenedorCarrusel = document.getElementById("ContenedorCarrusel");
+  const cadenaCategorias = Object.keys(productosCompletos);
 
-  for(let i=0; i<3; i++){
-    let categoriaC;
+  for(let i=0;i<3;i++){
+    let categoria;
     if(i === 0){
-      categoriaC = productosCompletos[CadenaCategorias[11]]; // AQUÍ EL CAMBIO IMPORTANTE
-    } else {
-      categoriaC = productosCompletos[CadenaCategorias[i]];
+      categoria=productosCompletos[cadenaCategorias[11]];
     }
-    const nombreC = categoriaC.nombres[0];
-    const imagenC = categoriaC.imagenes[0];
+    else{
+      categoria=productosCompletos[cadenaCategorias[i]];
+    }
+    const nombre=categoria.nombre[0];
+    const imagen=categoria.imagen[0];
 
     const item = document.createElement("div");
     item.classList.add("carousel-item");
     if(i === 0) item.classList.add("active"); // Para que el carrusel inicie bien
 
     item.innerHTML = `
-    <img src="${imagenC}" class="d-block w-100" alt="${nombreC}">
+    <img src="${imagen}" class="d-block w-100" alt="${nombre}">
     <div class="carousel-caption d-none d-md-block">        
     </div>
   `;
 
     contenedorCarrusel.appendChild(item);
+
+
+
   }
 
-});
+})
