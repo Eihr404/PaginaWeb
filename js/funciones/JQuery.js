43a6a917cb4ec04671b74438e33e9642d937f17e
DaniellaPR <pauladanielapozo@gmail.com>
2025-11-07 08:17:20 -0500

    Despliegue de información en botón Inicio

--- js/funciones/JQuery.js
+++ js/funciones/JQuery.js
@@ -1,19 +1,26 @@
-/*Contactos e Informacion*/
-
-$(document).ready(function(){
-  $("#btnContacto").on("click", function(){
+$(document).ready(function() {
+  $("#btnContacto").on("click", function() {
     const cuadro = $("#cuadro");
     cuadro.html("<p><strong>Cargando...</strong></p>").fadeIn(300);
-
-    // Simulamos carga de información
     setTimeout(() => {
       cuadro.html("<p>Información de contacto: <br> Tel: 0999999999</p>");
     }, 2000);
+    setTimeout(() => {
+      cuadro.fadeOut(500);
+    }, 6000);
+  });
+
+
+  $("#btnInicio").on("click", function(e) {
+    e.preventDefault();
+
+    const cuadro2 = $("#cuadro2");
+    cuadro2.html("<p><strong>Cargando...</strong></p>").fadeIn(300);
+    setTimeout(() => {
+      cuadro2.html("<p>Inicio Exitoso <br> Puede agregar productos al carrito</p>");
+    }, 2000);
+    setTimeout(() => {
+      cuadro2.fadeOut(500);
+    }, 4000);
   });
 });
-
-$(document).ready(function(){
-  $("#form-datos".on("submit", function(){
-    this.preventDefault();
-  }))
-})
